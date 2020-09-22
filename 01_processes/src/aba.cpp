86d8fc095606040daf02dc0cf768f117374f9997
= <=>
2020-09-22 10:58:41 +0200

    adding a signal handler to avoid zombies

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -3,9 +3,22 @@
 #include <thread>
 #include <chrono>
 #include <csignal>
+#include <sys/wait.h>
 
 
 using namespace std;
+
+void signal_handler(int){
+    quick_exit(EXIT_SUCCESS);
+}
+
+void children(std::chrono::milliseconds sleeptime){
+    while (true) {
+        cout << "A" << flush;
+        std::this_thread::sleep_for(sleeptime);
+        signal(SIGTERM, signal_handler);
+    }
+}
 
 int main(){
     auto pid{fork()};
@@ -15,18 +28,18 @@ int main(){
     int cntr{};
 
     if (pid == 0) {
-        while (true) {
-            cout << "A" << flush;
-            std::this_thread::sleep_for(sleeptime);
-        }
+        children(sleeptime);
     } else {
         while (cntr < 6) {
             cout << "B" << flush;
             std::this_thread::sleep_for(sleeptime);
             cntr++;
         }
-        kill(pid, SIGKILL);
+        int status{};
+        kill(pid, SIGTERM);
+        waitpid(pid, &status, 0);
         cout << endl;
     }
+    sleep(10);
 
 }
