89c08226a79c9181e5b551fed9e0bb867d5237bf
= <=>
2020-09-22 10:03:11 +0200

    fork process and print A for the Child and B for the Parent

---
+++ 01_processes/src/aba.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+
+
+using namespace std;
+
+int main(){
+    auto pid{fork()};
+
+    if (pid == 0) {
+        while (true) {
+            cout << "A" << flush;
+            sleep(1);
+        }
+    } else {
+        while (true) {
+            cout << "B" << flush;
+            sleep(1);
+        }
+    }
+
+}
