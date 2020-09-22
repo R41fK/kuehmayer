3eb6bc08750c50cffd1eaf392615775558d619d1
= <=>
2020-09-22 10:18:23 +0200

    killing child-process after 3s

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <thread>
 #include <chrono>
+#include <csignal>
 
 
 using namespace std;
@@ -11,16 +12,21 @@ int main(){
 
     std::chrono::milliseconds sleeptime(500);
 
+    int cntr{};
+
     if (pid == 0) {
         while (true) {
             cout << "A" << flush;
             std::this_thread::sleep_for(sleeptime);
         }
     } else {
-        while (true) {
+        while (cntr < 6) {
             cout << "B" << flush;
             std::this_thread::sleep_for(sleeptime);
+            cntr++;
         }
+        kill(pid, SIGKILL);
+        cout << endl;
     }
 
 }
