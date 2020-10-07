456158d8565ae40d5649b0bee134baf6b3f5377d
= <=>
2020-10-07 12:26:44 +0200

    use lock_guard

--- 03_sync1/include/Account.h
+++ 03_sync1/include/Account.h
@@ -1,9 +1,12 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 
+#include <mutex>
+
 class Account{
 
     int balance{};
+    std::mutex m{};
 
     public:
         int get_balance();
--- 03_sync1/src/Account.cpp
+++ 03_sync1/src/Account.cpp
@@ -1,12 +1,19 @@
 #include "../include/Account.h"
 
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
 
 void Account::deposit(int amount) {
     this->balance += amount;
 }
 
 bool Account::withdraw(int amount) {
+    lock_guard guard{this->m};
     if (this->balance - amount >= 0) {
+        this_thread::yield();
         this->balance -= amount;
         return true;
     }
