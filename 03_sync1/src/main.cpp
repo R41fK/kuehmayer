c401179ae364e5c8d502cad4aa82cd37d33479fe
= <=>
2020-10-07 12:21:08 +0200

    add threads

--- 03_sync1/src/main.cpp
+++ 03_sync1/src/main.cpp
@@ -1,6 +1,7 @@
 #include "../include/Account.h"
 
 #include <iostream>
+#include <thread>
 
 using namespace std;
 
@@ -8,14 +9,29 @@ int main() {
     
     Account my_bank_account;
 
-    my_bank_account.deposit(15);
+    // Punkt 1
+    // my_bank_account.deposit(15);
 
-    cout << "my new Account balance is: " << my_bank_account.get_balance() << endl;
+    // cout << "my new Account balance is: " << my_bank_account.get_balance() << endl;
 
-    cout << (my_bank_account.withdraw(6) ? "Money was withdrawn" : "Money was not withdrawn") 
-        << " new Balance is: " << my_bank_account.get_balance() << endl;
-        
-    cout << (my_bank_account.withdraw(10) ? "Money was withdrawn" : "Money was not withdrawn") 
-        << " new Balance is: " << my_bank_account.get_balance() << endl;
+    // cout << (my_bank_account.withdraw(6) ? "Money was withdrawn" : "Money was not withdrawn") 
+    //     << " new Balance is: " << my_bank_account.get_balance() << endl;
 
+    // cout << (my_bank_account.withdraw(10) ? "Money was withdrawn" : "Money was not withdrawn") 
+    //     << " new Balance is: " << my_bank_account.get_balance() << endl;
+
+    my_bank_account.deposit(1);
+
+    thread first{[&](){
+        my_bank_account.withdraw(1);
+    }};
+
+    thread second{[&](){
+        my_bank_account.withdraw(1);
+    }};
+
+    first.join();
+    second.join();
+
+    cout << "new Account balance is: " << my_bank_account.get_balance() << endl;
 }
