9bcefc1b91b4756c178f8aece1d67375ad93dbf9
= <=>
2020-10-20 08:33:07 +0200

    implementing loadsim

--- 04_sync2/src/loadsim.cpp
+++ 04_sync2/src/loadsim.cpp
@@ -2,10 +2,21 @@
 #include "../include/WorkQueue.h"
 
 #include <iostream>
+#include <thread>
 
 
 using namespace std;
 
 int main() {
-    return 0;
+    
+    WorkQueue queue{};
+
+    for (int i=0;; i++) {
+        WorkPacket packet{i};
+        queue.push(packet);
+        cout << "B: Submitted work packet " << packet.getId() << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+    
+
 }
