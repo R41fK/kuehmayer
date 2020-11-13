caa93d653035a0d85daf787a49f9bed264fe8188
= <=>
2020-11-13 11:05:54 +0100

    use template method for println

--- 05_sync3/src/philosopher.cpp
+++ 05_sync3/src/philosopher.cpp
@@ -7,35 +7,41 @@
 
 using namespace std;
 
-mutex out_mtx{};
+recursive_mutex out_mtx;
 
-void println(const initializer_list<string>& st){
-    out_mtx.lock();
-    for(string str : st){
-        cout << str << " ";
-    }
+
+void println() {
+    lock_guard<recursive_mutex> lg{out_mtx};
     cout << endl;
-    out_mtx.unlock();
 }
+
+
+template<typename T, typename... Rest>
+    void println(const T& word, const Rest&... rest) {
+    lock_guard<recursive_mutex> lg{out_mtx};
+    cout << word << ' ';
+    println(rest...);
+}
+
 
 
 void philospher::operator()(){
 
-    println({"Philosopher", to_string(this->id),  "is thinking ..."});
+    println("Philosopher", to_string(this->id),  "is thinking ...");
 
     this_thread::sleep_for(chrono::seconds(1));
 
-    println({"Philosopher", to_string(this->id), "attempts to get left fork"});
+    println("Philosopher", to_string(this->id), "attempts to get left fork");
 
     this->left_fork.lock();
 
-    println({"Philosopher", to_string(this->id), "got get left fork. Now he wants the right one ..."});
+    println("Philosopher", to_string(this->id), "got get left fork. Now he wants the right one ...");
 
     this->right_fork.lock();
 
     this_thread::sleep_for(chrono::seconds(2));
 
-    println({"Philosopher", to_string(this->id), "finished eating"});
+    println("Philosopher", to_string(this->id), "finished eating");
 
     this->left_fork.unlock();
     this->right_fork.unlock();
