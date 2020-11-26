c4ce2e02a8122e76ec7c9a8f8050a729f2c52be3
= <=>
2020-11-26 16:02:27 +0100

    using InfInt

--- 06_future/src/main.cpp
+++ 06_future/src/main.cpp
@@ -2,14 +2,20 @@
 
 #include "CLI11.hpp"
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmisleading-indentation"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#include "InfInt.h"
+#pragma GCC diagnostic pop
+
 using namespace std;
 
 
 
 string validation(const string& str){
     size_t found{str.find_first_not_of("0123456789")};
-    
-    if (found <= str.length()){
+
+    if (found <= str.length()) {
         return str + " is not a Integer";
     }
     return "";
@@ -26,4 +32,12 @@ int main(int argc, char* argv[]) {
 
     CLI11_PARSE(app, argc, argv);
 
+    vector<InfInt> factoring{};
+
+    for (string str : numbers) {
+        factoring.push_back(str);
+        cout << factoring.back() << endl;
+    }
+    
+    
 }
