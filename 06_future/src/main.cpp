12aaea7d41a2bb6a9f4f3c7d95664b8ad3de1228
= <=>
2020-11-26 15:43:27 +0100

    first implementation of validation

--- 06_future/src/main.cpp
+++ 06_future/src/main.cpp
@@ -4,18 +4,28 @@
 
 using namespace std;
 
+
+
+string validation(const string& str){
+    try{
+        stoi(str);
+    } catch (const invalid_argument & e) {
+        return str + " is not a Number";
+    } catch (const std::out_of_range & e) {
+        return str + " is to big for Int";
+    }
+    return "";
+}
+
+
 int main(int argc, char* argv[]) {
     
     CLI::App app("factoring");
 
-    vector<int> numbers{}; 
+    vector<string> numbers{}; 
 
-    app.add_option("numbers", numbers, "Numbers that are factored")->required();
+    app.add_option("numbers", numbers, "Numbers that are factored")->required()->check(validation);
 
     CLI11_PARSE(app, argc, argv);
-
-    for (int i : numbers) {
-        cout << i << endl;
-    }
 
 }
