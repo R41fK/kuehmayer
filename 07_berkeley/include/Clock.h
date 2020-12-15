193afab5ef145d355f0ba854d9a91fc87d6d424a
= <=>
2020-12-15 09:26:08 +0100

    adding second constructor

--- 07_berkeley/include/Clock.h
+++ 07_berkeley/include/Clock.h
@@ -2,6 +2,8 @@
 
 #include <chrono>
 
+#include "timeutils.h"
+
 class Clock
 {
 private:
@@ -12,8 +14,13 @@ public:
     Clock(std::string name): 
     name{name},
     curr_time{std::chrono::system_clock::now()}
-    {}
-    ;
+    {};
+
+    Clock(std::string name, int hours_, int minutes_, int seconds_):
+    name{name},
+    curr_time{set_time(curr_time, hours_, minutes_, seconds_)}
+    {};
+
    
    void operator()();
 };
