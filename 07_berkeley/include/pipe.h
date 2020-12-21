79f573d40e876f8e2a3ac38e46e2b1541c0d8fdb
= <=>
2020-12-21 14:27:05 +0100

    first implementation of pipe

---
+++ 07_berkeley/include/pipe.h
@@ -0,0 +1,45 @@
+#ifndef PIPE_H
+#define PIPE_H
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+template <typename T>
+class Pipe {
+    std::queue<T> backend;
+    std::mutex mtx;
+    std::condition_variable not_empty;
+    bool closed{false};
+  public:
+    Pipe& operator<<(T value) {
+        std::lock_guard lg{mtx};
+        if (closed){
+            return *this;
+        }
+        backend.push(value);
+        not_empty.notify_one();
+        return *this;
+    }
+    
+    Pipe& operator>>(T& value) {
+        std::unique_lock ul{mtx};
+        if (closed){
+            return *this;
+        }
+        not_empty.wait(ul, [this](){return this->backend.size});
+        value = backend.back();
+        backend.pop(value);
+        return *this;
+    }
+
+    void close() {
+        std::lock_guard lg{mtx};
+        closed = true;
+    }
+    
+    explicit operator bool() {
+        return closed;
+    }
+};
+#endif
