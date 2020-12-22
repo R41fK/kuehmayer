658576e743245ae5e0de8df2ae5582ff25feb489
= <=>
2020-12-22 11:08:10 +0100

    added cli

--- 07_berkeley/src/main.cpp
+++ 07_berkeley/src/main.cpp
@@ -1,20 +1,42 @@
 #include <thread>
 
+#include "CLI11.hpp"
 #include "TimeSlave.h"
 #include "TimeMaster.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+    bool set_monoton{false};
+    unsigned long latency1{0};
+    unsigned long latency2{0};
+    int deviation1{0};
+    int deviation2{0};
+    int deviationm{0};
     
-    TimeSlave s1{"slave1", 0, 0, 0, 0};
-    TimeSlave s2{"slave2", 0, 0, 0, 0};
-    TimeMaster m1{"master", 0, 0, 0, 0};
+    CLI::App app("berkeley");
 
-    s1.get_channel()->set_latency(1);
-    s2.get_channel()->set_latency(0);
+    app.add_flag("--monotone", set_monoton, "set monotone mode");
 
-    s2.set_time_monoton(true);
+    app.add_option("--latency1", latency1, "latency to channel 1 (both directions)");
+    app.add_option("--latency2", latency2, "latency to channel 1 (both directions)");
+
+    app.add_option("--deviation1", deviation1, "deviation of clock of slave 1");
+    app.add_option("--deviation2", deviation2, "deviation of clock of slave 2");
+    app.add_option("--deviationm", deviationm, "deviation of clock of master");
+
+    CLI11_PARSE(app, argc, argv);
+
+    TimeSlave s1{"slave1", 0, 0, 0, deviation1};
+    TimeSlave s2{"slave2", 0, 5, 0, deviation2};
+    TimeMaster m1{"master", 0, -6, 0, deviationm};
+
+    s1.get_channel()->set_latency(latency1);
+    s2.get_channel()->set_latency(latency2);
+
+    s1.set_time_monoton(set_monoton);
+    s2.set_time_monoton(set_monoton);
+    m1.set_time_monoton(set_monoton);
 
     m1.set_channel1(s1.get_channel());
     m1.set_channel2(s2.get_channel());
