4d2553703627ccad313efd86bf5997a46c258ec7
= <=>
2021-01-20 15:45:01 +0100

    adding CLI11

--- 08_daytime/src/daytime_client/client.cpp
+++ 08_daytime/src/daytime_client/client.cpp
@@ -3,15 +3,20 @@
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/bundled/color.h"
 #include "asio.hpp"
+#include "CLI11.hpp"
 
 using namespace asio;
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
     
     // spdlog::info(fmt::format(
     //             fg(fmt::color::linen), 
     //             "Hello world in linen."));
+
+    CLI::App app("client");
+
+    CLI11_PARSE(app, argc, argv);
 
     ip::tcp::iostream strm{"localhost", "1113"};
 
